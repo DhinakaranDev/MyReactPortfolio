d4b477479a62a2a214d446c8b88888c9815d5707
DhinakaranDev <166213395+DhinakaranDev@users.noreply.github.com>
2024-04-11 21:21:07 +0530

    Revert "name changed"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="py-4 bg-secondary text-white text-center">
-      &copy; MDcode 2024
+      &copy; DKcode 2024
     </div>
   );
 }
