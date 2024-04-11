bcab632da1b5305a3dc9a90584ace67d7eed6f76
DhinakaranDev <166213395+DhinakaranDev@users.noreply.github.com>
2024-04-11 21:37:30 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="py-4 bg-secondary text-white text-center">
-      &copy; DKcode 2024
+      &copy; DK-code 2024
     </div>
   );
 }
